71f70ecf8253f5e854be9af1e5b137dc1543656f
chippawah <charles.cantrell@me.com>
2023-06-15 14:51:38 -0700

    refactor for better test table naming; use t.Run

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,21 +3,24 @@ package structs
 import "testing"
 
 type tableTest struct {
+	name  string
 	shape Shape
 	want  float64
 }
 
 func TestArea(t *testing.T) {
 	areaTests := []tableTest{
-		{shape: Rectangle{12, 6}, want: 72.0},
-		{shape: Circle{10}, want: 314.1592653589793},
-		{shape: Triangle{12, 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, want: 36.30},
 	}
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
 	}
 }
 
