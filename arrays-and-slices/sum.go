0bb419899f3ad5b7e04e2c4777d27e1c1a866188
chippawah <charles.cantrell@me.com>
2023-06-14 23:30:11 -0700

    adds sumall fn

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,9 +1,18 @@
-package arrays
+package arraysandslices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numSets ...[]int) (sumSet []int) {
+	numSetLen := len(numSets)
+	sumSet = make([]int, numSetLen)
+	for idx, numSet := range numSets {
+		sumSet[idx] = Sum(numSet)
+	}
+	return
 }
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,14 +1,30 @@
-package arrays
+package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("it should sum a collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+func TestSumAll(t *testing.T) {
+	numSetA := []int{1, 1}
+	numSetB := []int{1, 2}
+	numSetC := []int{2, 2, 2}
+	got := SumAll(numSetA, numSetB, numSetC)
+	want := []int{2, 3, 6}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
