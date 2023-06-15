bbe0f2c110cec3519eeb462172a7c867291c6a75
chippawah <charles.cantrell@me.com>
2023-06-14 19:22:35 -0700

    fixes iteration pkg

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,10 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(char string) (repeated string) {
-	for i := 0; i < repeatCount; i++ {
+func Repeat(char string, count int) (repeated string) {
+	if count <= 1 {
+		count = 1
+	}
+	for i := 0; i < count; i++ {
 		repeated += char
 	}
 	return
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,18 +1,38 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("It should return the string if the number is less than 1", func(t *testing.T) {
+		repeated := Repeat("a", -1)
+		expected := "a"
 
+		assertCorrectString(t, repeated, expected)
+	})
+	t.Run("It should repeat the string the number of times given", func(t *testing.T) {
+		repeated := Repeat("b", 2)
+		expected := "bb"
+		assertCorrectString(t, repeated, expected)
+	})
+}
+
+func assertCorrectString(t *testing.T, repeated, expected string) {
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
+func ExampleRepeat() {
+	repeated := Repeat("c", 5)
+	fmt.Println(repeated)
+	// Output: ccccc
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 100)
 	}
 }
