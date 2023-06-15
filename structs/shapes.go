0c3670de0066b561cdd7eb0c5f9ceec78c541871
chippawah <charles.cantrell@me.com>
2023-06-15 13:17:12 -0700

    adds area fns and tests. uses methods and interfaces

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,32 @@
 package structs
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width, height float64) float64 {
-	return (width * height)
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return (rectangle.Width * rectangle.Height)
+}
+
+func (rectangle Rectangle) Perimeter() float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * math.Pow(circle.Radius, 2)
+}
+
+func (circle Circle) Circumference() float64 {
+	return 2 * math.Pi * circle.Radius
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,20 +2,42 @@ package structs
 
 import "testing"
 
-func TestShapes(t *testing.T) {
-	checkResult := func(t *testing.T, got, want float64) {
+func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			t.Errorf("got %g want %g", got, want)
 		}
 	}
-	t.Run("calculate the permiter of a rectangle", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		want := 100.0
+		checkArea(t, rectangle, want)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+}
+
+func TestPerimeter(t *testing.T) {
+	checkResult := func(t *testing.T, got, want float64) {
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
 		want := 40.0
 		checkResult(t, got, want)
 	})
-	t.Run("calulate the area of a rectangle", func(t *testing.T) {
-		got := Area(12.0, 6.0)
-		want := 72.0
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Circumference()
+		want := 62.83185307179586
 		checkResult(t, got, want)
 	})
 }
