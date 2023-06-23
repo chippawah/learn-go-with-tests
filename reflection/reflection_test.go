6421a94b42c687284a7b0f2c6b186fa058af3cee
chippawah <charles.cantrell@me.com>
2023-06-23 00:22:02 -0700

    allow for extra fields

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,20 +1,43 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+type TableTestCase struct {
+	Name          string
+	Input         interface{}
+	ExpectedCalls []string
+}
 
 func TestWalk(t *testing.T) {
-	expected := "Foo"
-	var got []string
-	x := struct {
-		Name string
-	}{expected}
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-	if len(got) != 1 {
-		t.Errorf("wrong number of fn calls, got %d want 1", len(got))
+	cases := []TableTestCase{
+		{
+			Name: "struct with one string field",
+			Input: struct {
+				Name string
+			}{"Foobarius"},
+			ExpectedCalls: []string{"Foobarius"},
+		},
+		{
+			Name: "struct with two string fields",
+			Input: struct {
+				Name string
+				City string
+			}{"Foobarius", "Boston"},
+			ExpectedCalls: []string{"Foobarius", "Boston"},
+		},
 	}
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+	for _, testCase := range cases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			var got []string
+			walk(testCase.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, testCase.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, testCase.ExpectedCalls)
+			}
+		})
 	}
 }
